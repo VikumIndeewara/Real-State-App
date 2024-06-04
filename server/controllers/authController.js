f2247032a1559f9901c49c3759f3dd9e23467d32
VikumIndeewara <vikum.i.bandara@gmail.com>
2024-06-05 02:54:32 +0530

    created sign in api route and tested with postman

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,7 @@
 import { User } from "../Models/UserModel.js";
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from "../utils/error.js";
 
 export const signUp = async(req,res,next)=>{
     const {username,email,password} = req.body;
@@ -12,6 +14,27 @@ export const signUp = async(req,res,next)=>{
     }catch(error){
         next(error);
     }
+
+};
+
+export const signIn = async(req,res,next)=>{
+    const {email,password}= req.body;
+    try{
+    const validUser = await User.findOne({email});
+    if (!validUser) return next(errorHandler(404,'User not found!'));
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if (!validPassword) return next(errorHandler(401,'Password Incorrect!'));
+    //we save logged in users ID as a cookie for later use
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+    //we exclude the password because we don't need to send it to user with cooke data
+    const { password: pass, ...rest } = validUser._doc;// This will log the plain object containing the document data exclude additional Mongoose-specific properties and methods. 
+    res
+      .cookie('access_token', token, { httpOnly: true })//httpOnly prevent access of other third party applications, we can set cookie expire time also
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 
 };
 
