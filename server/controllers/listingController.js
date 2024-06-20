632d6b00c7a89c19e23ae917f68da7e26d7dfcea
VikumIndeewara <vikum.i.bandara@gmail.com>
2024-06-21 01:43:44 +0530

    created(listing route) search listing api fuctionality

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -46,3 +46,23 @@ export const deleteListing=async(req,res,next)=>{
         next(err)
     }
  }
+
+ export const searchListings=async(req,res,next)=>{
+    try{
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';//sort by descending order
+
+        const listings = await Listing.find({
+            propertyname:{$regex:searchTerm, $options:'i'},//regex search for a keyword anywhere in a paragrapg and options makes lower or uppercase
+        })
+        .sort({[sort]:order})
+        .limit(limit)
+        .skip(startIndex);
+        return res.status(200).json(listings);
+    }catch(err){
+        next(err)
+    }
+ }
--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createListing,deleteListing,getListing, updateListing } from '../controllers/listingController.js';
+import { createListing,deleteListing,getListing, searchListings, updateListing } from '../controllers/listingController.js';
 
 const router = express.Router();
 
+router.get('/search-listings',searchListings);
 router.get('/:id',getListing);
 router.post('/create-listing',createListing);
 router.delete('/delete-listing/:id',deleteListing);
 router.put('/update-listing/:id',updateListing);
+
 
 export default router;
