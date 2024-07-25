6d155fe8b1da5e92cc0c8b83d4edd76526d114be
VikumIndeewara <vikum.i.bandara@gmail.com>
2024-07-25 20:41:27 +0530

    Added pre installation

--- server/index.js
+++ server/index.js
@@ -8,11 +8,15 @@ import userRouter from '../server/routes/userRoute.js'
 import authRouter from '../server/routes/authRoute.js'
 import listingRouter from '../server/routes/listingRoute.js'
 import cors from 'cors';
+import path from 'path';
 
 const corsOptions = {
     origin: 'http://localhost:5173', 
     credentials: true, 
   };
+
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -23,6 +27,12 @@ app.use(cookieParser());
 app.use('/user',userRouter);
 app.use('/auth',authRouter);
 app.use('/listing',listingRouter);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
+
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal-server-error';
