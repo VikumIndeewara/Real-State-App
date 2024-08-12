50a76f624aa453d6d031b4b2d09fb74da5c46b3a
VikumIndeewara <vikum.i.bandara@gmail.com>
2024-08-12 18:50:04 +0530

    updated api link

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/server_api':{
-        target:'http://localhost:5555',
+        target:'https://real-state-app-server.onrender.com',
         secure:false,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/server_api/, ''),
